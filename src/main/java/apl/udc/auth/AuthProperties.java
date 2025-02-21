d400294c4e5f1e204d478daef18051e5f5e6b731
kych4n <hades1@kakao.com>
2025-02-21 11:09:07 +0900

    Refactor [#7] secretKey -> seed

--- src/main/java/apl/udc/auth/AuthProperties.java
+++ src/main/java/apl/udc/auth/AuthProperties.java
@@ -6,6 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public record AuthProperties(
         String username,
         String password,
-        String secretKey
+        String seed
 ) {
 }
