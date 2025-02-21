7f74d8f5666ca212ea550b6de43ff788f229a744
kych4n <hades1@kakao.com>
2025-02-21 11:15:29 +0900

    Feat [#7] 암호화 데이터 다운로드 및 복호화

--- src/main/java/apl/udc/api/facade/MainFacade.java
+++ src/main/java/apl/udc/api/facade/MainFacade.java
@@ -1,15 +1,25 @@
 package apl.udc.api.facade;
 
-import apl.udc.api.dto.AttributeResponse;
+import apl.udc.api.dto.request.InfoForFilteringRequest;
+import apl.udc.api.dto.response.AttributeResponse;
+import apl.udc.auth.AuthProperties;
+import apl.udc.global.common.OdcProperties;
+import apl.udc.global.util.Decryptor;
+import apl.udc.global.util.FileDownloader;
+import apl.udc.global.util.SavePath;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.IOException;
+import java.time.Duration;
+import javax.crypto.SecretKey;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.springframework.stereotype.Component;
 
@@ -19,12 +29,16 @@ import org.springframework.stereotype.Component;
 public class MainFacade {
 
     private final Gson gson = new Gson();
-    private final OkHttpClient client = new OkHttpClient();
+    private final OkHttpClient client = OkHttpClient();
+    private final FileDownloader fileDownloader;
+    private final AuthProperties authProperties;
+    private final OdcProperties odcProperties;
+    private final Decryptor decryptor;
 
     public AttributeResponse getAttributes(String authorization) throws IOException {
 
         Request request = new Request.Builder()
-                .url("http://localhost:8080/api/v1")
+                .url(odcProperties.address() + "/api/v1")
                 .header("Authorization", authorization)
                 .get()
                 .build();
@@ -42,4 +56,46 @@ public class MainFacade {
             }
         }
     }
+
+    public String getEncryptedDataUrl(String authorization, InfoForFilteringRequest infoForFilteringRequest)
+            throws IOException {
+
+        RequestBody requestBody = RequestBody.create(gson.toJson(infoForFilteringRequest),
+                MediaType.get("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+                .header("Authorization", authorization)
+                .url(odcProperties.address() + "/api/v1")
+                .post(requestBody)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                JsonObject jsonObject = JsonParser.parseString(response.body().string()).getAsJsonObject();
+                return jsonObject.getAsJsonObject("data").get("signedUrl").getAsString();
+            } else {
+                log.info("Request Failed.");
+                return null;
+            }
+        }
+    }
+
+    public void download(String encryptedDataUrl) throws IOException {
+        fileDownloader.downloadFile(encryptedDataUrl, SavePath.ENCRYPTED.getPath());
+    }
+
+    public void decrypt() throws Exception {
+        SecretKey secretKey = decryptor.generateKey(authProperties.seed());
+        decryptor.decrypt(secretKey);
+    }
+
+    private OkHttpClient OkHttpClient() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(Duration.ofMinutes(5L))
+                .callTimeout(Duration.ofMinutes(5L))
+                .readTimeout(Duration.ofMinutes(5L))
+                .writeTimeout(Duration.ofMinutes(5L))
+                .build();
+    }
+
 }
