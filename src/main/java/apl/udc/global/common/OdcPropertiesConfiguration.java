96f450aabbff122811bbbe36ec6424846c9c5f07
kych4n <hades1@kakao.com>
2025-02-21 11:06:19 +0900

    Feat [#7] ODC 정보 속성 매핑

---
+++ src/main/java/apl/udc/global/common/OdcPropertiesConfiguration.java
@@ -0,0 +1,9 @@
+package apl.udc.global.common;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan(basePackages = "apl.udc.global.common")
+public class OdcPropertiesConfiguration {
+}
